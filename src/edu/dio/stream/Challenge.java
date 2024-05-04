98b579bca979962f1c9745b40aff9e777eb4ec00
Diego Nunes <diegodnunes12@gmail.com>
2024-05-04 17:26:40 -0300

    outras funcoes

--- src/edu/dio/stream/Challenge.java
+++ src/edu/dio/stream/Challenge.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 public class Challenge {
     public static void main(String[] args) {
+        // https://github.com/digitalinnovationone/ganhando_produtividade_com_Stream_API_Java/tree/master/src/stream_api
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        // ordenando a lista
+        // 1-  ordenando a lista
         //numbers.stream().sorted().forEach(System.out::println);
 
-        // Soma dos numeros pares
+        // 2 - Soma dos numeros pares
         int result = numbers.stream().filter(number -> number % 2 == 0).reduce(0, Integer::sum);
         System.out.println(result);
     }
